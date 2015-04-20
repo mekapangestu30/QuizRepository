e514f181c35aba3f7092ce713339e90be1e0e71e
123040128 <123040128@mail.unpas.ac.id>
2015-04-20 12:35:15 +0700

    Tambah Biodata

--- src/main/java/org/ifunpas/rkppl/quiz_123040128/App.java
+++ src/main/java/org/ifunpas/rkppl/quiz_123040128/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Quiz Praktikum RKPPL" );
+        System.out.println("BIODATA");
         System.out.println("Nama : Muhamad Eka Pangestu");
         System.out.println("NRP : 123040128");
         System.out.println("Shift Praktikum : Senin, 11.00-13.00");
